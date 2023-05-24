5c361a5f96aa530493b926b1ced65874953bca6f
Yany Choi <dydghks0818@naver.com>
2023-05-24 15:59:37 +0900

    feat: Mailing 서비스 Kafka 토픽 목록 추가 및 메일 DTO 타입 추가

--- mailing/src/consumer.py
+++ mailing/src/consumer.py
@@ -2,26 +2,32 @@ from aiokafka import AIOKafkaConsumer
 from logger import get_logger
 import os
 from mail import Mail
-from dto.mail_dto import MailDto
+from utils.mail_type import MailType
+from dto.user_create_event_dto import UserCreateEventDto
+from dto.entry_campaign_event_dto import EntryCampaignEventDto
 
-async def consume(logger = get_logger(), kafka_host: str = os.getenv("KAFKA_HOST"), kafka_group: str = os.getenv("KAFKA_GROUP_ID"), kafka_topic: str = os.getenv("KAFKA_TOPIC")):
+async def consume(logger = get_logger(), kafka_host: str = os.getenv("KAFKA_HOST"), kafka_group: str = os.getenv("KAFKA_GROUP_ID")):
     consumer = AIOKafkaConsumer(
-        kafka_topic,
+        "user-creation",
+        "campaign-entry",
+        "campaign-draw",
         bootstrap_servers=kafka_host,
         group_id=kafka_group,
-        # "mailing",
         # bootstrap_servers='localhost:9092',
-        # group_id='mailiing'
+        # group_id='mailing'
         )
     
     mail = Mail()
     await consumer.start()
     try:
         async for msg in consumer:
-            print(f"""{msg.value.decode('utf-8')}""")
             logger.debug(f"Consumed: {msg.value.decode('utf-8')}")
-            msg_json: MailDto = MailDto.parse_raw(msg.value.decode('utf-8'))
-            mail.send(msg_json)
+            # logic separated by topic type
+            if msg.topic == "user-creation":
+                msg_json: UserCreateEventDto = UserCreateEventDto.parse_raw(msg.value.decode('utf-8'))
+            elif msg.topic == "campaign-entry":
+                msg_json: EntryCampaignEventDto = EntryCampaignEventDto.parse_raw(msg.value.decode('utf-8'))
+            mail.send(msg_json, MailType(msg.topic))
     finally:
         await consumer.stop()
         logger.debug("Consumer stopped")
--- mailing/src/mail.py
+++ mailing/src/mail.py
@@ -3,25 +3,67 @@ import os
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from logger import get_logger
-from dto.mail_dto import MailDto
-from content.apply import MailContent
+from utils.mail_type import MailType
 
+from dto.user_create_event_dto import UserCreateEventDto
+from dto.entry_campaign_event_dto import EntryCampaignEventDto
+from dto.draw_campaign_event_dto import DrawCampaignEventDto
 class Mail:
     def __init__(self):
         self.smtp_host = os.getenv("MAIL_HOST")
         self.smtp_port = os.getenv("MAIL_PORT")
         self.sender_email = os.getenv("MAIL_USERNAME")
         self.password = os.getenv("MAIL_PASSWORD")
+
         self.ssl_context = ssl.create_default_context()
         self.logger = get_logger()
 
-    def send(self, kafka_event: MailDto):
-        message = MailContent(kafka_event.mail_type).get_content(kafka_event.mail_receiver)
+    def generate_message(self, receiver_mail: str):
+        message = MIMEMultipart("alternative")
+        message["Subject"] = self.subject
+        message["From"] = self.sender_email
+        message["To"] = receiver_mail
+
+        # Turn these into plain/html MIMEText objects
+        html_mime = MIMEText(self.content, "html")
+
+        # Add HTML/plain-text parts to MIMEMultipart message
+        # The email client will try to render the last part first
+        message.attach(html_mime)
+
+        return message
+
+    def send(self, kafka_event: UserCreateEventDto | EntryCampaignEventDto | DrawCampaignEventDto, event_type: MailType):
+        self.get_content(kafka_event, event_type)
+        message = self.generate_message(kafka_event.email)
 
         # Create secure connection with server and send email
         with smtplib.SMTP_SSL(self.smtp_host, self.smtp_port, context=self.ssl_context) as server:
             server.login(self.sender_email, self.password)
             server.sendmail(
-                self.sender_email, kafka_event.mail_receiver, message.as_string()
+                self.sender_email, kafka_event.email, message.as_string()
             )
-            self.logger.debug(f"Mail sent to: {kafka_event.mail_receiver}")
+            self.logger.debug(f"Mail sent to: {kafka_event.email}")
+
+    def get_content(self, kafka_event: UserCreateEventDto | EntryCampaignEventDto | DrawCampaignEventDto, event_type: MailType):
+        if (event_type == MailType.CAMPAIGN_ENTRY):
+            kafka_event: EntryCampaignEventDto
+            with open("content/campaign_entry.html", "r") as f:
+                self.subject = "[갓생] 상품에 응모해주셔서 감사합니다!"
+                self.content = f.read().format(
+                    username=kafka_event.nickname
+                )
+        elif (type == MailType.CAMPAIGN_DRAW):
+            kafka_event: DrawCampaignEventDto
+            with open("content/campaign_draw.html", "r") as f:
+                self.subject = "[갓생] 상품 당첨을 축하드립니다!"
+                self.content = f.read().format(
+                    username=kafka_event.nickname
+                )
+        elif (event_type == MailType.USER_CREATION):
+            kafka_event: UserCreateEventDto
+            with open("content/user_creation.html", "r") as f:
+                self.subject = "[갓생] 회원가입을 축하드립니다!"
+                self.content = f.read().format(
+                    username=kafka_event.nickname
+                )
