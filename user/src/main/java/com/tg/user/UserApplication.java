d4edef9c341a074d47e81c2ae2b9f1b89490d8ed
JinuCheon <woojin8787@gmail.com>
2023-05-25 19:34:52 +0900

    feat: DB 생성일 추가

--- user/src/main/java/com/tg/user/UserApplication.java
+++ user/src/main/java/com/tg/user/UserApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @ImportAutoConfiguration({FeignAutoConfiguration.class})
 @EnableFeignClients("com.tg.user.user.infra")
+@EnableJpaAuditing
 public class UserApplication {
 
     public static void main(String[] args) {
