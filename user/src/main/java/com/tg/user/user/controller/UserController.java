5b777c60bbb8bfd62e06f76c00c63f9adf27e24f
JinuCheon <woojin8787@gmail.com>
2023-05-25 22:30:06 +0900

    refactor: 유저 조회에서 본인 정보 조회 시 0을 입력하도록 변경

--- user/src/main/java/com/tg/user/user/controller/UserController.java
+++ user/src/main/java/com/tg/user/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.tg.user.user.controller;
 
 import com.tg.user.auth.UserContext;
+import com.tg.user.auth.domain.SessionUserVo;
 import com.tg.user.user.domain.UserService;
 import com.tg.user.user.domain.dto.UserInformationResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
@@ -10,7 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "USER", description = "유저 API")
@@ -24,8 +24,11 @@ public class UserController {
     @GetMapping("/{targetUserId}")
     public ResponseEntity<UserInformationResponseDto> getUserInfo(@PathVariable Long targetUserId) {
         if (targetUserId == 0) {
-            targetUserId = UserContext.getContext().getId();
-            if(targetUserId == null) return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            SessionUserVo context = UserContext.getContext();
+            if(context == null) {
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+            targetUserId = context.getId();
         }
         return ResponseEntity.ok().body(userService.getUserInformation(targetUserId));
     }
