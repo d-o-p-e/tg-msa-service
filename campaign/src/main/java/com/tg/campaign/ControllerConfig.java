b46c12639416ae5add8815cd8460630c5abfd5e7
JinuCheon <woojin8787@gmail.com>
2023-05-27 18:34:59 +0900

    feat: exception handler 추가

---
+++ campaign/src/main/java/com/tg/campaign/ControllerConfig.java
@@ -0,0 +1,20 @@
+package com.tg.campaign;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+@Slf4j
+public class ControllerConfig {
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public void handle(HttpMessageNotReadableException e) {
+        log.warn("Returning HTTP 400 Bad Request", e);
+        throw e;
+    }
+}
